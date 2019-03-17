2bc5f51bbd219c5a74809a9d6b9fa182c2c68c28
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-03-17 13:44:14 -0500

    parsers works in dual mode: run / test

--- src/auto_martini/__main__.py
+++ src/auto_martini/__main__.py
@@ -15,9 +15,9 @@
   @originally written by: Tristan BEREAU (bereau at mpip-mainz.mpg.de)
   @modified by: Andrew Abi-Mansour (andrew.gaam at gmail.com)
 
-  Auto_MARTINI: a tool for automatic MARTINI mapping and parametrization of small organic molecules                                               
+  Auto_Martini: a tool for automatic MARTINI mapping and parametrization of small organic molecules                                               
     
-  Auto_MARTINI is open-source, distributed under the terms of the GNU Public
+  Auto_Martini is open-source, distributed under the terms of the GNU Public
   License, version 2 or later. It is distributed in the hope that it will
   be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
@@ -32,33 +32,54 @@ import argparse
 import logging
 
 from auto_martini.solver import cg_molecule
-from auto_martini.topology import gen_molecule_sdf
+from auto_martini.topology import gen_molecule_smi, gen_molecule_sdf
 
-parser = argparse.ArgumentParser(description='Map atomistic structure to MARTINI mapping',
-                                 epilog='Tristan BEREAU (2014); Andrew Abi-Mansour (2019)')
-group = parser.add_mutually_exclusive_group(required=True)
+import sys
+
+
+def checkArgs(args):
+
+  if not args.sdf and not args.smi:
+    parser.error("run requires --sdf or --smi")
+    if not args.mol:
+      parser.error("run requires --mol")
+
+parser = argparse.ArgumentParser(prog='auto_martini', description='Generates Martini force field for atomistic structures of small organic molecules',
+                                formatter_class=argparse.RawDescriptionHelpFormatter,
+                                 epilog='''Developers:\n===========\nTristan Bereau (bereau [at] mpip-mainz.mpg.de)\nAndrew Abi-Mansour (andrew.gaam [at] gmail.com)''')
+
+parser.add_argument('mode', type=str, help='run or test auto_martini', choices=['run','test'])
+
+group = parser.add_mutually_exclusive_group(required=False)
+
 group.add_argument('--sdf', dest='sdf', type=str, required=False, help='SDF file of atomistic coordinates')
 group.add_argument('--smi', dest='smi', type=str, required=False, help='SMILES string of atomistic structure')
-parser.add_argument('--mol', dest='molname', type=str, required=True, help='Name of CG molecule')
+parser.add_argument('--mol', dest='molname', type=str, required=False, help='Name of CG molecule')
 parser.add_argument('--aa', dest='aa', type=str, help='output all-atom structure to .gro file')
 parser.add_argument('--cg', dest='cg', type=str, help='output coarse-grained structure to .gro file')
 parser.add_argument('-v', '--verbose', dest='verbose', action='count', default=0, help='increase verbosity')
 parser.add_argument('--fpred', dest='forcepred', action='store_true', help='verbose')
 
+if len(sys.argv)==1:
+    parser.print_help(sys.stderr)
+    sys.exit(1)
+
 args = parser.parse_args()
-if args.verbose >= 2:
-    level = logging.DEBUG
-elif args.verbose >= 1:
-    level = logging.INFO
-else:
-    level = logging.WARNING
 
-logging.basicConfig(filename='auto_martini.log', format='%(asctime)s:%(levelname)s: %(message)s', level=level)
+if args.mode == 'run':
+  checkArgs(args)
 
-# Generate molecule's structure from SDF or SMILES
-if args.sdf:
-    mol = gen_molecule_sdf(args.sdf)
-else:
-    mol = gen_molecule_smi(args.smi) 
+  if args.verbose >= 2:
+      level = logging.DEBUG
+  elif args.verbose >= 1:
+      level = logging.INFO
+  else:
+      level = logging.WARNING
 
-cg_molecule(mol, args.molname, args.aa, args.cg, args.forcepred)
+  # Generate molecule's structure from SDF or SMILES
+  if args.sdf:
+      mol = gen_molecule_sdf(args.sdf)
+  else:
+      mol = gen_molecule_smi(args.smi) 
+
+  cg_molecule(mol, args.molname, args.aa, args.cg, args.forcepred)
