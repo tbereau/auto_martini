91cf0c0b84ff612dee04cd3d4f862ebaa1cb8175
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-10-12 02:32:56 -0400

    version read from _version.py

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+
+## [0.0.1]
+### Added
+
+- New CLI for reading input and writing output
+- New modular design consisting of "test" and "engine" submodules
+- CLI arg --top to the parser so that the output topology would be written to a file (no longer printed to stdout)
+- Support for python 3
+- New "optimization.pyx" module for faster implementation (in C) of find_bead_pos()
+
+### Changed
+- Fixed genMoleculeSDF() which did not use "sdf" input argument
+- Fixed function printBonds() which used undefined "atomPartitioning" object
+- Function Substructure() no longer creates then reads "tmp-auto-martini.smi" file
+- Function check_additivity() no longer creates then reads "tmp-auto-martini.smi" file
+
+### Removed
+- Undefined objects "hbondA" and "hbondD" in function printAtoms()
+- Variable "mad" in function checkAdditivity() was already defined as a function
--- setup.py
+++ setup.py
@@ -1,44 +1,35 @@
 '''
-  Created on March 17, 2019 by Andrew Abi-Mansour
+Created on March 17, 2019 by Andrew Abi-Mansour
 
-          _    _ _______ ____    __  __          _____ _______ _____ _   _ _____ 
-     /\  | |  | |__   __/ __ \  |  \/  |   /\   |  __ \__   __|_   _| \ | |_   _|
-    /  \ | |  | |  | | | |  | | | \  / |  /  \  | |__) | | |    | | |  \| | | |  
-   / /\ \| |  | |  | | | |  | | | |\/| | / /\ \ |  _  /  | |    | | | . ` | | |  
-  / ____ \ |__| |  | | | |__| | | |  | |/ ____ \| | \ \  | |   _| |_| |\  |_| |_ 
- /_/    \_\____/   |_|  \____/  |_|  |_/_/    \_\_|  \_\ |_|  |_____|_| \_|_____|
-                                                                                 
-                                                                                 
-  @originally written by: Tristan BEREAU (bereau at mpip-mainz.mpg.de)
-  @modified by: Andrew Abi-Mansour (andrew.gaam at gmail.com)
+This is the::
 
-  Auto_Martini: a tool for automatic MARTINI mapping and parametrization of small organic molecules                                               
-    
-  Auto_Martini is open-source, distributed under the terms of the GNU Public
-  License, version 2 or later. It is distributed in the hope that it will
-  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
-  received a copy of the GNU General Public License along with PyGran.
-  If not, see http://www.gnu.org/licenses . See also top-level README
-  and LICENSE files.
+ █████╗ ██╗   ██╗████████╗ ██████╗         ███╗   ███╗ █████╗ ██████╗ ████████╗██╗███╗   ██╗██╗
+██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗        ████╗ ████║██╔══██╗██╔══██╗╚══██╔══╝██║████╗  ██║██║
+███████║██║   ██║   ██║   ██║   ██║        ██╔████╔██║███████║██████╔╝   ██║   ██║██╔██╗ ██║██║
+██╔══██║██║   ██║   ██║   ██║   ██║        ██║╚██╔╝██║██╔══██║██╔══██╗   ██║   ██║██║╚██╗██║██║
+██║  ██║╚██████╔╝   ██║   ╚██████╔╝███████╗██║ ╚═╝ ██║██║  ██║██║  ██║   ██║   ██║██║ ╚████║██║
+╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝╚═╝  ╚═══╝╚═╝
+                                                                                               
+toolkit for automatic MARTINI mapping and parametrization of small organic molecules
 
-  Github link to original repo: https://github.com/tbereau/auto_martini
+Auto_Martini is developed by:
+Tristan BEREAU (bereau at mpip-mainz.mpg.de)
+Andrew Abi-Mansour (andrew.gaam at gmail.com)
 
-  Some BUGS fixed + improvements:
-
-  - function printAtoms() uses undefined objects "hbondA" and "hbondD"
-  - function checkAdditivity() defined "mad" variable which is already defined as a function
-  - function genMoleculeSDF() did not use "sdf" input argument
-  - function printBonds() used undefined "atomPartitioning" object
-  - function Substructure() no longer creates then reads "tmp-auto-martini.smi" file
-  - function check_additivity() no longer creates then reads "tmp-auto-martini.smi" file
-  - Added --top arg to the parser so that the output topology would be written to a file (no longer printed to stdout)
-  - Added support for python 3
-  - Created a new "optimization.pyx" module for faster implementation (in C) of find_bead_pos() routine
-
+Auto_Martini is open-source, distributed under the terms of the GNU Public License, version 2 or later. 
+It is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have received a copy of the 
+GNU General Public License along with PyGran. If not, see http://www.gnu.org/licenses . See also top-level 
+README and LICENSE files.
 '''
 
 import sys, os
+
+# Extract metadata from simulation._version
+with open(os.path.join('src', 'auto_martini', '_version.py'), 'r') as fp:
+        for line in fp.readlines():
+                if '__version__' in line:
+                        __version__ = line.split('=')[-1].strip().strip("''")
 
 class Installer(object):
 
@@ -97,22 +88,7 @@ except:
   with Installer(repo='https://github.com/rdkit/rdkit') as _:
     pass
 
-# import auto_martini __version__ from src
-path = os.path.abspath(__file__).split(__file__)[0] + 'src/auto_martini/__init__.py'
 
-if sys.version_info[0] < 3:
-  import imp
-  version_mod = imp.load_source('__init__.py', path)
-elif sys.version_info[1] <= 3:
-  from importlib.machinery import SourceFileLoader
-  version_mod = SourceFileLoader('__init__.py', path).load_module() # this works in v3.6.7 too
-else: # v3.4 and above
-  import importlib.util
-  spec = importlib.util.spec_from_file_location('__init__.py', path)
-  version_mod = importlib.util.module_from_spec(spec)
-  spec.loader.exec_module(version_mod)
-
-__version__ =  version_mod.__version__
 
 if __name__ == '__main__':
 
